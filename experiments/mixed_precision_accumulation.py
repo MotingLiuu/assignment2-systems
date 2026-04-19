59667cb6f48ac2084a412087b2d7deca14b86cfe
MotingLiuu <moting.liuu@gmail.com>
2026-04-19 10:27:54 +0800

    added mixed_precision_accumulation script

---
+++ experiments/mixed_precision_accumulation.py
@@ -0,0 +1,21 @@
+import torch
+
+s = torch.tensor(0, dtype=torch.float32)
+for i in range(1000):
+    s += torch.tensor(0.01, dtype=torch.float32)
+print(f"Single precision sum float32: {s.item()}")
+
+s = torch.tensor(0, dtype=torch.float16)
+for i in range(1000):
+    s += torch.tensor(0.01, dtype=torch.float16)
+print(f"Half precision sum float16: {s.item()}")
+
+s = torch.tensor(0, dtype=torch.float32)
+for i in range(1000):
+    s += torch.tensor(0.01, dtype=torch.float16)
+print(f"Mixed precision sum float32 + float16: {s.item()}")
+    
+s = torch.tensor(0, dtype=torch.float16)
+for i in range(1000):
+    s += torch.tensor(0.01, dtype=torch.float32)
+print(f"Mixed precision sum float16 + float32: {s.item()}")
